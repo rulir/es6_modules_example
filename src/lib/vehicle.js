d21d129430dcc5f0d43d0248884c27fcc1ae85c7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-01-24 12:29:45 +0530

    Add vehicle

---
+++ src/lib/vehicle.js
@@ -0,0 +1,14 @@
+export default class {
+  constructor(type, number) {
+    this.type = type;
+    this.number = number;
+  }
+
+  display() {
+    return `Number: ${this.number}`;
+  }
+}
+
+export const print = function(v) {
+  console.log(`Vehicle Info: ${v.display()}`);
+};
