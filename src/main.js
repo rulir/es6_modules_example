5e4283df9bf2fedc3acc1b1740415ded3c449eb7
Daniil Holodnyi <daniil.holodnyi@craftmann.ru>
2016-04-05 18:39:51 +0300

    export mult

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {sum} from './lib/math';
 import Vehicle from './lib/vehicle';
+import {print} from './lib/vehicle';
 
 const v1 = new Vehicle('Car', 'GH-3355'); 
-console.log(v1.display());
+
+const log = print(v1);
 
 const total = sum(10, 20);
 console.log(total);
